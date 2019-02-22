44e74964a58303c1514189b85fa2ba2237f269b9
radiocity <radiocity@bk.ru>
2019-02-22 17:44:17 +0300

    chore: tmp

---
+++ test/compiler.js
@@ -0,0 +1,54 @@
+import path from 'path';
+import webpack from 'webpack';
+import MemoryFS from 'memory-fs';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+export default (fixture = 'index.twig', options = {}) => {
+  const context = path.resolve(__dirname, 'fixtures');
+  const entry = path.resolve(context, 'index.js');
+  const template = path.resolve(context, fixture);
+  const compiler = webpack({
+    mode: 'production',
+    context,
+    entry,
+    output: {
+      path: path.resolve(__dirname),
+      filename: '[name].bundle.js',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.twig$/,
+          use: [
+            {
+              loader: '../../src/index.js',
+              options,
+            },
+          ],
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template,
+        inject: false,
+        filename: path.resolve(__dirname, path.basename(template)),
+      }),
+    ],
+  });
+
+  compiler.outputFileSystem = new MemoryFS();
+
+  return new Promise((resolve, reject) => {
+    compiler.run((errors, stats) => {
+      if (errors || stats.hasErrors()) {
+        const config = {
+          chunks: false,
+          colors: true,
+        };
+        reject(errors || stats.toString(config));
+      }
+      resolve(stats);
+    });
+  });
+};
